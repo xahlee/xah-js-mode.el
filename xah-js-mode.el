b9faf907fd9d9fab37832ab73a37f73db7f2b24d
Xah Lee <xah@xahlee.org>
2014-06-17 22:07:00 -0700

    much ado about nothing

--- xah-js-mode.el
+++ xah-js-mode.el
@@ -1,4 +1,4 @@
-;;; xah-js-mode.el --- Major mode for editing emacs lisp. -*- coding: utf-8 -*-
+;;; xah-js-mode.el --- Major mode for editing JavaScript. -*- coding: utf-8 -*-
 
 ;; Copyright © 2013 by Xah Lee
 
@@ -17,6 +17,16 @@
 ;; version 0.2, 2013-12-25 added array methods and string method highlighting
 ;; version 0.1, 2013-08-21 first version
 
+;; TODO
+;; rid of calling js-mode
+;; make sure the comment works
+;; create complete syntax table
+;; add new faces
+;; separate diff types of keywords to use diff face
+;; add completion
+;; add indentation
+;; add support for autocomplete
+
 (defvar xah-js-mode-hook nil "Standard hook for `xah-js-mode'")
 
 (defvar xjs-js-lang-words nil "a list of JavaScript keywords.")
@@ -31,7 +41,6 @@
 "delete"
 "do"
 "else"
-"false"
 "finally"
 "for"
 "function"
@@ -39,12 +48,10 @@
 "in"
 "instanceof"
 "new"
-"null"
 "return"
 "switch"
 "this"
 "throw"
-"true"
 "try"
 "typeof"
 "var"
@@ -52,8 +59,15 @@
 "while"
 "with"
 
+"call"
+"isExtensible"
+"preventExtensions"
+"getOwnPropertyDescriptor"
+"isSealed"
+"isFrozen"
+"freeze"
+
 "substring"
-"undefined"
 
 "Array"
 "Boolean"
@@ -63,7 +77,6 @@
 "Function"
 "Infinity"
 "JSON"
-"Math"
 "NaN"
 "Number"
 "Object"
@@ -118,8 +131,27 @@
 "volatile"
 "yield"
 
+"alert"
+
+"writable"
+"enumerable"
+"configurable"
+
 ;; --------------------
-"charCodeAt"
+
+"toString"
+
+"create"
+"getPrototypeOf"
+"isPrototypeOf"
+"defineProperty"
+"value"
+
+"prototype"
+
+"getOwnPropertyNames"
+"hasOwnProperty"
+
 "apply"
 "arguments"
 "getBoundingClientRect"
@@ -127,28 +159,10 @@
 "firstChild"
 "nodeValue"
 
- "length"
+"constructor"
+"length"
 
-"forEach"
-
-"Math.floor"
-
-"console.log"
-"document.createElement"
-"innerHTML"
-"hasChildNodes"
-"lastChild"
-"removeChild"
-"document"
-
-"parentNode"
-"appendChild"
-
-"String.fromCodePoint"
-"toString"
-"style"
-"setAttribute"
-"createTextNode"
+"JSON.stringify"
 
 ) )
 
@@ -174,7 +188,7 @@
 "sort"
 "splice"
 "toLocalString"
-"toString"
+
 "unshift"
 ) )
 
@@ -198,22 +212,96 @@
 "toLocaleLowerCase"
 "charAt"
 "charCodeAt"
-"toString"
+
 "toValueOf"
 "localeCompare"
 ) )
 
-(defvar xjs-dom-words nil "a list of keywords more or less related to emacs system.")
+(defvar xjs-js-math-methods nil "a list of JavaScript Math methods.")
+(setq xjs-js-math-methods '(
+"Math.abs"
+"Math.acos"
+"Math.acosh"
+"Math.asin"
+"Math.asinh"
+"Math.atan"
+"Math.atanh"
+"Math.atan2"
+"Math.cbrt"
+"Math.ceil"
+"Math.clz32"
+"Math.cos"
+"Math.cosh"
+"Math.exp"
+"Math.expm1"
+"Math.floor"
+"Math.fround"
+"Math.hypot"
+"Math.imul"
+"Math.log"
+"Math.log1p"
+"Math.log10"
+"Math.log2"
+"Math.max"
+"Math.min"
+"Math.pow"
+"Math.random"
+"Math.round"
+"Math.sign"
+"Math.sin"
+"Math.sinh"
+"Math.sqrt"
+"Math.tan"
+"Math.tanh"
+"Math.toSource"
+"Math.trunc"
+) )
+
+(defvar xjs-dom-words nil "a list of keywords from DOM or browser.")
 (setq xjs-dom-words '(
+
+"style"
+"setAttribute"
+"createTextNode"
+
 "addEventListener"
 "removeEventListener"
-"document.getElementById"
-"document.getElementsByTagName"
+"getElementById"
+"getElementsByTagName"
+"getElementsByClassName"
+
+"onmouseover"
+
+"console.log"
+"createElement"
+"innerHTML"
+"hasChildNodes"
+"lastChild"
+"removeChild"
+"document"
+
+"parentNode"
+"appendChild"
 ) )
 
 (defvar xjs-keyword-builtin nil "a list of js  names")
 (setq xjs-keyword-builtin '(
+) )
 
+(defvar xjs-constants nil "a list of constants")
+(setq xjs-constants '(
+"null"
+"undefined"
+"true"
+"false"
+"Math.E"
+"Math.LN2"
+"Math.LN10"
+"Math.LOG2E"
+"Math.LOG10E"
+"Math.PI"
+"Math.SQRT1_2"
+"Math.SQRT2"
 ) )
 
 (defvar xjs-js-vars-1 nil "a list js variables names")
@@ -225,33 +313,41 @@
 
 (setq xjs-font-lock-keywords
       (let (
+          (jsMathMethods (regexp-opt xjs-js-math-methods 'symbols) )
           (domWords (regexp-opt xjs-dom-words 'symbols) )
-          (emacsBuiltins (regexp-opt xjs-keyword-builtin 'symbols) )
+          (jsBuildins (regexp-opt xjs-keyword-builtin 'symbols) )
           (jsLangWords (regexp-opt xjs-js-lang-words 'symbols) )
           (jsVars1 (regexp-opt xjs-js-vars-1 'symbols) )
           (jsArrayMethods (regexp-opt xjs-js-array-methods 'symbols) )
           (jsStrMethods (regexp-opt xjs-js-str-methods 'symbols) )
-
-)
+          (jsConstants (regexp-opt xjs-constants 'symbols) )
+          )
         `(
+          (,jsMathMethods . font-lock-keyword-face)
+          (,jsConstants . font-lock-constant-face)
           (,domWords . font-lock-function-name-face)
-          (,emacsBuiltins . font-lock-type-face)
+          (,jsBuildins . font-lock-type-face)
           (,jsLangWords . font-lock-keyword-face)
           (,jsArrayMethods . font-lock-keyword-face)
           (,jsStrMethods . font-lock-keyword-face)
           (,jsVars1 . font-lock-variable-name-face)
           ) ) )
 
-;;font-lock-comment-delimiter-face
-;;font-lock-comment-face
-;;font-lock-doc-face
-;;font-lock-negation-char-face
-;;font-lock-preprocessor-face
-;;font-lock-reference-face
-;;font-lock-string-face
-;;font-lock-type-face
-;;font-lock-variable-name-face
-;;font-lock-warning-face
+;; fontfont-lock-builtin-face
+;; font-lock-comment-delimiter-face
+;; font-lock-comment-face
+;; font-lock-constant-face
+;; font-lock-doc-face
+;; font-lock-function-name-face
+;; font-lock-keyword-face
+;; font-lock-negation-char-face
+;; font-lock-preprocessor-face
+;; font-lock-reference-face
+;; font-lock-string-face
+;; font-lock-syntactic-face-function
+;; font-lock-type-face
+;; font-lock-variable-name-face
+;; font-lock-warning-face
 
 
 ;; keybinding
